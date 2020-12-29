ad1d951cf2a371c094fe3794490964933a28639a
Facundo De nardo <facundodenardo@MacBook-Pro-de-Facundo-5.local>
2020-12-29 11:33:35 +0100

    refactor(services) refactor services .js to typescript

---
+++ frontend/src/Service/rent-booking.service.ts
@@ -0,0 +1,17 @@
+class bookingService {
+    public bookings: Booking[];
+    constructor() {
+        this.bookings = [];
+    }
+
+    findLocalBookingById = (booking: Booking) => this.bookings.find(_bookings => _bookings.bookingId === booking.bookingId);
+
+    add = (booking: Booking) => {
+        const bookingSearched = this.findLocalBookingById(booking);
+        
+      if (!bookingSearched){
+        this.bookings = [...this.bookings, booking];
+      }
+
+    }
+}
